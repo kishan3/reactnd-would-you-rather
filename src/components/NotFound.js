65f62687c415be36320d4bb64c5246c673947beb
Kishan Mehta <kishan@mobifly.co.uk>
2018-09-10 14:34:50 +0530

    Add notfound component.

---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class PageNotFound extends Component {
+    render() {
+        return (
+            <div>
+                <h3>The question you are looking for is NOT FOUND!</h3>
+            </div>
+        )
+    }
+}
+
+export default PageNotFound;
